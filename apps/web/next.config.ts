5796d6bee26b047b245ba0c671b4ecd279a75d14
Jeremias Santos <jeremiassantos@Mac-mini-de-Jeremias.local>
2026-01-28 10:24:19 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@lofi-study-app/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
